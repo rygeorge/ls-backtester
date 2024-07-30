83833b81dcd72459534f3dd92cdcd5a7e2ea51a8
rygeorge <ryan.george2@mail.mcgill.ca>
2024-07-29 23:12:11 -0400

    sql script

--- data/-- SQLite.sql
+++ data/-- SQLite.sql
@@ -1,3 +1,11 @@
 -- SQLite
-SELECT * 
-FROM stock_data;
+SELECT  df.Ticker,
+        date(df.Date) as dt,
+        round(df.Open,2) as Open,
+        round(df.High, 2) as High,
+        round(df.Low, 2) as Low,
+        round(df.Close, 2) as Close,
+        round(df.Adj_Close, 2) as Adj_Close,
+        df.Volume
+FROM stock_data AS df
+ORDER BY df.Ticker ASC, dt DESC;
